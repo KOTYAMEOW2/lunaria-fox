13a946dc542e66bd16ae78dd92caf3958372a0c9
KOTYAMEOW2 <maksim.2005andreev1@gmail.com>
2026-05-01 08:40:48 +0300

    Delete app directory

--- app/dashboard/[guildId]/stalcraft/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default async function LegacyGuildStalcraftPage({ params }: { params: Promise<{ guildId: string }> }) {
-  const { guildId } = await params;
-  redirect(`/dashboard/${guildId}`);
-}
